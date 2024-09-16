280f80a68959d22e18f6d6758ea48f942862bac7
Vicente <vicenteferrer_01@hotmail.com>
2024-09-16 09:49:59 -0300

    W01 Express and EJS Templates

--- server.js
+++ server.js
@@ -6,14 +6,92 @@
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+require("dotenv").config()
+
 const app = express()
+
 const static = require("./routes/static")
+
+// Index content
+const VEHICLE = "DMC DeLorean"
+
+const index = {
+  title: "Home",
+  hero: {
+    content: {
+      title: VEHICLE,
+      features: ["3 Cup holders", "Superman doors", "Fuzzy dice!"]
+    },
+    img: {
+      alt: VEHICLE,
+      src: "delorean"
+    }
+  },
+  reviews: {
+    title: VEHICLE,
+    list: [
+      {
+        content: "So fast it's almost like traveling in time.",
+        rate: 4
+      },
+      {
+        content: "Coolest ride on the road.",
+        rate: 4
+      },
+      {
+        content: "I\'m feeling McFly!",
+        rate: 5
+      },
+      {
+        content: "The most futuristic ride of our way.",
+        rate: 4.5
+      },
+      {
+        content: "80's livin and I love it!",
+        rate: 5
+      }
+    ]
+  },
+  upgrades: {
+    title: VEHICLE,
+    list: [
+      {
+        src: "flux-cap.png",
+        alt: "Flux Capacitor"
+      },
+      {
+        src: "flame.jpg",
+        alt: "Flame Decals"
+      },
+      {
+        src: "bumper_sticker.jpg",
+        alt: "Bumper Stickers"
+      },
+      {
+        src: "hub-cap.jpg",
+        alt: "Hub Caps"
+      }
+    ]
+  }
+}
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", (req, res) => {
+  res.render("index", { ...index })
+})
 
 /* ***********************
  * Local Server Information
@@ -26,5 +104,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on http://${host}:${port}`)
 })
