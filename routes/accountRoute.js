c40d95eeec8c019f20c7ca4100b1339f82f71c0e
Vicente <vicenteferrer_01@hotmail.com>
2024-09-27 14:34:15 -0300

    Server-side Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const router = new Router()
 
 const accountController = require("../controllers/accountController")
-// const { registrationRules, checkRegData, loginRules, checkLogData } = require("../utilities/account-validation")
+const { registrationRules, checkRegData, loginRules, checkLogData } = require("../utilities/account-validation")
 
 const { handleErrors } = require("../utilities/")
 
@@ -10,6 +10,6 @@ router.get("/login", handleErrors(accountController.buildLogin))
 router.get("/registration", handleErrors(accountController.buildRegister))
 
 // router.post("/login", loginRules(), checkLogData, (req, res) => { res.status(200).send("login process") })
-router.post("/registration", handleErrors(accountController.registerAccount))
+router.post("/registration", registrationRules(), checkRegData, handleErrors(accountController.registerAccount))
 
 module.exports = router
---
+++ utilities/account-validation.js
@@ -0,0 +1,132 @@
+const { body, validationResult } = require("express-validator")
+
+const { getNav } = require(".")
+const { checkExistingEmail } = require("../models/account-model")
+
+const validate = {}
+
+/*  **********************************
+  *  Registration Data Validation Rules
+  * ********************************* */
+validate.registrationRules = () => ([
+    body("account_firstname")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isLength({ min: 1 })
+        .withMessage("Please provide a first name."),
+
+    body("account_lastname")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isLength({ min: 2 })
+        .withMessage("Please provide a last name."),
+
+    body("account_email")
+        .trim()
+        .escape()
+        .notEmpty()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+            const emailExists = await checkExistingEmail(account_email)
+            if (emailExists) {
+                throw new Error("Email exists. Please log in or use different email")
+            }
+        }),
+
+    body("account_password")
+        .trim()
+        .notEmpty()
+        .isStrongPassword({
+            minLength: 12,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        })
+        .withMessage("Password does not meet requirements.")
+])
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkRegData = async (req, res, next) => {
+    const { account_firstname, account_lastname, account_email } = req.body
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        const nav = await getNav()
+
+        res.render("account/registration", {
+            errors,
+            title: "Registration",
+            nav,
+            account_firstname,
+            account_lastname,
+            account_email,
+        })
+
+        return
+    }
+
+    next()
+}
+
+// /*  **********************************
+//   *  Login Data Validation Rules
+//   * ********************************* */
+// validate.loginRules = () => ([
+//     body("account_email")
+//         .trim()
+//         .escape()
+//         .notEmpty()
+//         .isEmail()
+//         .normalizeEmail()
+//         .withMessage("Use valid email is required.")
+//         .custom(async (account_email) => {
+//             const emailExists = await checkExistingEmail(account_email)
+//             if (!emailExists) {
+//                 throw new Error("Email does not exists. Please sign up or use different email")
+//             }
+//         }),
+
+//     body("account_password")
+//         .trim()
+//         .notEmpty()
+//         .isStrongPassword({
+//             minLength: 12,
+//             minLowercase: 1,
+//             minUppercase: 1,
+//             minNumbers: 1,
+//             minSymbols: 1
+//         })
+//         .withMessage("Password does not meet requirements.")
+// ])
+
+// /* ******************************
+//  * Check data and return errors or continue to login
+//  * ***************************** */
+// validate.checkLogData = async (req, res, next) => {
+//     const { account_email } = req.body
+//     const errors = validationResult(req)
+
+//     if (!errors.isEmpty()) {
+//         const nav = await getNav()
+
+//         res.render("account/login", {
+//             errors,
+//             title: "Login",
+//             nav,
+//             account_email
+//         })
+
+//         return
+//     }
+
+//     next()
+// }
+
+module.exports = validate
