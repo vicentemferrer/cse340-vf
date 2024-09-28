6523e8f96c3a41e8542f2d30a05b7ae530153e54
Vicente <vicenteferrer_01@hotmail.com>
2024-09-27 22:10:46 -0300

    Vehicle Validation Succesfully Solved

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -31,42 +31,42 @@ async function getInventoryItemById(item_id) {
     }
 }
 
-// async function addClassification(classification_name) {
-//     try {
-//         const sql = "INSERT INTO public.classification (classification_name) VALUES ($1) RETURNING *"
+async function addClassification(classification_name) {
+    try {
+        const sql = "INSERT INTO public.classification (classification_name) VALUES ($1) RETURNING *"
 
-//         return await pool.query(sql, [classification_name])
-//     } catch (error) {
-//         return error.message
-//     }
-// }
+        return await pool.query(sql, [classification_name])
+    } catch (error) {
+        return error.message
+    }
+}
 
-// async function checkExistingClassification(classification_name) {
-//     try {
-//         const sql = "SELECT * FROM public.classification WHERE classification_name = $1"
-//         const { rowCount } = await pool.query(sql, [classification_name])
+async function checkExistingClassification(classification_name) {
+    try {
+        const sql = "SELECT * FROM public.classification WHERE classification_name = $1"
+        const { rowCount } = await pool.query(sql, [classification_name])
 
-//         return rowCount
-//     } catch (error) {
-//         return error.message
-//     }
-// }
+        return rowCount
+    } catch (error) {
+        return error.message
+    }
+}
 
-// async function addVehicle(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) {
-//     try {
-//         const sql = "INSERT INTO public.inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *"
+async function addVehicle(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) {
+    try {
+        const sql = "INSERT INTO public.inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *"
 
-//         return await pool.query(sql, [inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id])
-//     } catch (error) {
-//         return error.message
-//     }
-// }
+        return await pool.query(sql, [inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id])
+    } catch (error) {
+        return error.message
+    }
+}
 
 module.exports = {
     getClassifications,
     getInventoryByClassificationId,
     getInventoryItemById,
-    // addClassification,
-    // checkExistingClassification,
-    // addVehicle
+    addClassification,
+    checkExistingClassification,
+    addVehicle
 }
---
+++ utilities/inventory-validation.js
@@ -0,0 +1,169 @@
+const { body, validationResult } = require("express-validator")
+
+const { getNav, buildClassificationList } = require(".")
+const { checkExistingClassification } = require("../models/inventory-model")
+
+/*  **********************************
+  *  Classification Data Validation Rules
+  * ********************************* */
+
+function classificationRules() {
+    return ([
+        body("classification_name")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 3 })
+            .withMessage("Please provide a valid classification name.")
+            .custom(async (classification_name) => {
+                const classificationExists = await checkExistingClassification(classification_name)
+                if (classificationExists) {
+                    throw new Error("Classification exists. Please add a different classification.")
+                }
+            })
+    ])
+}
+
+/* ******************************
+ * Check data and return errors or continue
+ * ***************************** */
+async function checkClassData(req, res, next) {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        const nav = await getNav()
+
+        res.render("inventory/add-classification", {
+            errors,
+            title: "Add Classification",
+            nav
+        })
+
+        return
+    }
+
+    next()
+}
+
+/*  **********************************
+  *  Vehicle Data Validation Rules
+  * ********************************* */
+
+function vehicleRules() {
+    return ([
+        body("classification_id")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isInt({
+                gt: 0
+            })
+            .withMessage("Please select a vehicle classiffication."),
+
+        body("inv_make")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 3 })
+            .withMessage("Please provide a maker."),
+
+        body("inv_model")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 3 })
+            .withMessage("Please provide a model."),
+
+        body("inv_description")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 1 })
+            .withMessage("Please provide a description."),
+
+        body("inv_image")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 15 })
+            .withMessage("Please provide a valid path."),
+
+        body("inv_thumbnail")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 15 })
+            .withMessage("Please provide a valid path."),
+
+        body("inv_price")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isFloat({
+                gt: 0,
+                locale: 'en-US'
+            })
+            .withMessage("Please provide a price."),
+
+        body("inv_year")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isInt({
+                min: 1900,
+                max: new Date().getFullYear(),
+            })
+            .withMessage("Please provide a year."),
+
+        body("inv_miles")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isInt({
+                gt: 0,
+                allow_leading_zeroes: false
+            })
+            .withMessage("Please provide miles quantity."),
+
+        body("inv_color")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 3 })
+            .withMessage("Please provide a color.")
+    ])
+}
+
+/* ******************************
+ * Check data and return errors or continue to register vehicle
+ * ***************************** */
+async function checkVehicleData(req, res, next) {
+    const { classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color } = req.body
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        const nav = await getNav()
+
+        const classificationList = await buildClassificationList(classification_id)
+
+        res.render("inventory/add-vehicle", {
+            errors,
+            title: "Add Vehicle",
+            nav,
+            classificationList,
+            inv_make,
+            inv_model,
+            inv_description,
+            inv_price,
+            inv_year,
+            inv_miles,
+            inv_color
+        })
+
+        return
+    }
+
+    next()
+}
+
+module.exports = { classificationRules, checkClassData, vehicleRules, checkVehicleData }
