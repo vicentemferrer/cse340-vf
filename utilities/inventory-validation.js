433bc50f6e152bde2c653ba1c5287e3940d5073a
Vicente <vicenteferrer_01@hotmail.com>
2024-10-04 10:52:34 -0300

    Patch POST Add Vehicle

--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -138,7 +138,7 @@ function vehicleRules() {
  * Check data and return errors or continue to register vehicle
  * ***************************** */
 async function checkVehicleData(req, res, next) {
-    const { classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color } = req.body
+    const { classification_id, inv_make, inv_model, inv_description, inv_price, inv_year, inv_miles, inv_color } = req.body
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
